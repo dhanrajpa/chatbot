d64d5e3d767fa680a725496648c3af86b0b05997
maheshn <maheshn@cybage.com>
2022-11-29 18:20:14 +0530

    admin dashboard

---
+++ js/home.js
@@ -0,0 +1,58 @@
+let d = [];
+const fetchQuetion = async () => {
+  $.getJSON(
+    "http://localhost:4000/NewQueries",
+
+    function (data) {
+      var queries = "";
+      d = data;
+
+      //iterating through objects
+      $.each(data, function (key, value) {
+        //construction of rows
+        //having rows from the json object
+        
+        queries += "<tbody><tr>";
+        queries += "<td>" + (key + 1) + "</td>";
+        queries += "<td>" + value.query + "</td>";
+        queries +=
+          "<td>" +
+          '<button type="button" id="btn"  class="btn btn-danger ml-4" onclick="deleteQuery(this)" value=' +
+          key +
+          ">Delete</button>" +
+          "</td>";
+        queries += "<tr></tbody>";
+      });
+      //inserting rows into the table
+      $("#table").append(queries);
+    }
+  );
+};
+
+$(document).ready(function () {
+  //fetching data from json file
+  fetchQuetion();
+});
+
+//delete functionality
+
+async function deleteQuery(it) {
+  let text = "Are Your sure want to delete this Query?";
+  if (!confirm(text)) {
+    return;
+  } else {
+    const i = it.value;
+    console.log(i);
+    id = d[i].id;
+    await $.ajax({
+      url: `http://localhost:4000/NewQueries/${id}`,
+      type: "DELETE",
+      success: function (result) {
+       
+          $("td").remove();
+      },
+    });
+  }
+  await fetchQuetion();
+}
+
